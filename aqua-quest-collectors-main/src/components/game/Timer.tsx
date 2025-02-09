5e056c81b9a7faa51355a3003a79b92c8f34689a
PierreOlivierP <pierre-olivier.piquant@polymtl.ca>
2025-02-09 00:50:08 -0500

    ta

--- aqua-quest-collectors-main/src/components/game/Timer.tsx
+++ aqua-quest-collectors-main/src/components/game/Timer.tsx
@@ -1,37 +1,37 @@
 import { useState, useEffect } from "react";
 
 const Timer = ({ onSecondsChange }) => {
-    const [seconds, setSeconds] = useState(0);
-  
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setSeconds((prevSeconds) => {
-          const newSeconds = prevSeconds + 1;
-  
-          // Send updated seconds to the parent through the callback
-          if (onSecondsChange) {
-            onSecondsChange(newSeconds); // Notify parent with new seconds
-          }
-  
-          return newSeconds;
-        });
-      }, 1000);
-  
-      // Cleanup interval when Timer component unmounts
-      return () => clearInterval(intervalId);
-    }, [onSecondsChange]); // Dependency array makes sure the effect runs only once
-  
-    const formatTime = (time) => {
-      const minutes = Math.floor(time / 60);
-      const timeStarted = time % 60;
-      return `${minutes < 10 ? '0' : ''}${minutes}:${timeStarted < 10 ? '0' : ''}${timeStarted}`;
-    };
-  
-    return (
-      <div>
-        <h2>Timer: {formatTime(seconds)}</h2>
-      </div>
-    );
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        const newSeconds = prevSeconds + 1;
+
+        // Send updated seconds to the parent through the callback
+        if (onSecondsChange) {
+          onSecondsChange(newSeconds); // Notify parent with new seconds
+        }
+
+        return newSeconds;
+      });
+    }, 1000);
+
+    // Cleanup interval when Timer component unmounts
+    return () => clearInterval(intervalId);
+  }, [onSecondsChange]); // Dependency array makes sure the effect runs only once
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const timeStarted = time % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${timeStarted < 10 ? '0' : ''}${timeStarted}`;
   };
+
+  return (
+    <div>
+      <h2>Timer: {formatTime(seconds)}</h2>
+    </div>
+  );
+};
 
 export default Timer;
